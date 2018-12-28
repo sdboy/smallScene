2cb0bba1c7582fe3a19253e53e5fb22255c1ad73
jiaoguang <jiaoyiguang@gmail.com>
2018-12-28 15:53:44 +0800

    初始化

---
+++ src/main/java/com/kelan/smallscene/SmallsceneApplication.java
@@ -0,0 +1,14 @@
+package com.kelan.smallscene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallsceneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallsceneApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/kelan/smallscene/SmallsceneApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kelan.smallscene;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmallsceneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
