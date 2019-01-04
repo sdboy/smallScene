df13baa1949d2e233fbe96bcd912ab0ea87e892c
jiaoguang <jiaoyiguang@gmail.com>
2019-01-04 18:04:21 +0800

    配置数据库

--- src/main/java/com/kelan/smallscene/SmallsceneApplication.java
+++ src/main/java/com/kelan/smallscene/SmallsceneApplication.java
@@ -1,9 +1,11 @@
 package com.kelan.smallscene;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.kelan.smallscene.dao")
 public class SmallsceneApplication {
 
 	public static void main(String[] args) {
