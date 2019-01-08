3d4ddc9025a5c1e521b6ed073a7ca95fbbda46a0
jiaoguang <jiaoyiguang@gmail.com>
2019-01-08 11:00:48 +0800

    添加组织数逻辑

---
+++ src/main/java/com/kelan/smallscene/service/DepartmentService.java
@@ -0,0 +1,26 @@
+package com.kelan.smallscene.service;
+
+import com.kelan.smallscene.common.ServerResponse;
+import com.kelan.smallscene.entity.Department;
+
+/**
+ * <p></p>
+ *
+ * @author JG
+ * @version V1.0.0
+ * @date 2019/1/8 10:54
+ * @see
+ */
+public interface DepartmentService {
+  /**
+   * <p>添加组织信息</p>
+   * @method addDepartment
+   * @author JG
+   * @date 2019/1/8 10:56
+   * @param department 组织信息
+   * @return com.kelan.smallscene.common.ServerResponse<java.lang.String>
+   * @since V1.0.0
+   * @version V1.0.0
+   */
+  public ServerResponse<String> addDepartment(Department department);
+}
---
+++ src/main/java/com/kelan/smallscene/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,30 @@
+package com.kelan.smallscene.service.impl;
+
+import com.kelan.smallscene.common.ServerResponse;
+import com.kelan.smallscene.dao.DepartmentMapper;
+import com.kelan.smallscene.entity.Department;
+import com.kelan.smallscene.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p></p>
+ *
+ * @author JG
+ * @version V1.0.0
+ * @date 2019/1/8 10:57
+ * @see
+ */
+@Service
+public class DepartmentServiceImpl implements DepartmentService {
+
+  private final static Logger logger = LoggerFactory.getLogger(DepartmentServiceImpl.class);
+  @Autowired
+  private DepartmentMapper departmentMapper;
+  @Override
+  public ServerResponse<String> addDepartment(Department department) {
+    return null;
+  }
+}
