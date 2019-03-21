54cb5ff9b8dc541a52fb746933b3ccfe54d276f4
jiaoguang <jiaoyiguang@gmail.com>
2019-03-21 10:31:05 +0800

    格式化

--- smallscene-web/src/main/java/com/kelan/smallscene/SmallsceneApplication.java
+++ smallscene-web/src/main/java/com/kelan/smallscene/SmallsceneApplication.java
@@ -5,14 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
 
+import java.util.HashSet;
+
 @SpringBootApplication
 //@EnableJms
 @MapperScan(value = "com.kelan.smallscene.dao")
 public class SmallsceneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmallsceneApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SmallsceneApplication.class, args);
+  }
 
 }
 
